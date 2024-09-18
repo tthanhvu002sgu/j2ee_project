a6bc41f037851aeb7dd23e02b81b1a8f325a0e45
vu <3120410623@sv.sgu.edu.vn>
2024-09-18 20:19:57 +0700

    first commit

---
+++ src/test/java/com/example/j2ee/J2eeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.j2ee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class J2eeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
